2cda0a9b1656e71364cc013706dbf82d26f8dfde
josep <joseph_chiu@Outlook.com>
2024-01-29 15:20:43 +0800

    Included docstrings for functions (WIP)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pathlib import Path  # for Path.cwd()
 attrData: List[List[int]] = []
 attrNames: [List[str]] = []
 
-filePath = Path.cwd() / "brdrxingusc_dataset.csv"
+filePath = Path.cwd() / "TestCase3.csv"
 
 
 # anything with a SV. prefix refers to the class holding static variables
@@ -39,12 +39,17 @@ def displayBuses() -> None:
 
 # function for displaying user's input statistics
 def displayUserInput(userInput: str) -> None:
+    """ Displays user selected input for task 2
+
+    :param userInput:
+    :return:
+    """
     print()
     if userInput == '\n':
         return
     # convert option to index supported value A -> 0, B -> 1, C -> 2, D -> 3
     userIn: int = (ord(userInput) - ord('A'))
-    selected: List[int] = attrData[userIn][0:7]
+    selected: List[int] = attrData[userIn][0:8]
     sumSelection: int = sum(selected)
     maxSelection: int = max(selected)
     nameSelection: str = attrNames[userIn]
@@ -53,7 +58,7 @@ def displayUserInput(userInput: str) -> None:
 
     # print the mean number of the user's chosen variable
     print(f"The mean number of {nameSelection} crossing the border each year is "
-          f"{sumSelection / len(selected):.0f}")
+          f"{sumSelection / len(selected):.0f} (Truncated down to nearest whole number)")
 
     print(f"In {2000 + selected.index(maxSelection)} crossed the highest number of "
           f"{attrNames[userIn]} from the year 2000 to 2007 with "
@@ -65,6 +70,11 @@ def displayUserInput(userInput: str) -> None:
 
 
 def displayYrOnYr(userInput: str) -> None:
+    """
+
+    :param userInput: the user's input
+    :return:
+    """
     print()
     if userInput == '\n':
         return
@@ -101,6 +111,13 @@ def displayYrOnYr(userInput: str) -> None:
 
 
 def displayPlot() -> None:
+    """ Displays custom plots for graph plotting
+
+    See https://matplotlib.org/stable/users/index for matplotlib function documentation \n
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplot.html#matplotlib.pyplot.subplot \n
+    https://matplotlib.org/stable/api/ticker_api.html#module-matplotlib.ticker \n
+    :return:
+    """
     # 2 subplots, using constrained layout (scales plot to fit every element on the figure)
     fig, axis = plt.subplots(2, layout="constrained")
     busPerPerson: List[float] = [passenger / attrData[1][index]
@@ -108,19 +125,19 @@ def displayPlot() -> None:
     personalVehiclesThousands: List[float] = [vehicles / 1000 for vehicles in attrData[2]]
     # The following code below was written with reference to documentation on matplotlib.org
     # Plot the graph for first subplot
-    axis[0].plot(range(2000, 2013), busPerPerson, label="Bus passengers per bus")
-    axis[0].set_title("Traffic data of bus passengers per bus from 2000 to 2012")
+    axis[0].plot(range(2000, 2013), busPerPerson, label=f"{attrNames[0].capitalize()} per {attrNames[1].lower()}")
+    axis[0].set_title(f"Traffic data of {attrNames[0].lower()} per {attrNames[1].lower()} from 2000 to 2012")
     axis[0].grid()
-    axis[0].set(xlabel="year", ylabel="Bus passengers per bus")
+    axis[0].set(xlabel="year", ylabel=f"{attrNames[0].capitalize()} per {attrNames[1].lower()}")
     axis[0].xaxis.set_major_locator(tkr.MultipleLocator(1))
     axis[0].yaxis.set_minor_locator(tkr.MultipleLocator(1))
     axis[0].legend()
-    axis[1].bar(range(2000, 2013), personalVehiclesThousands, linewidth=1, label="Number of personal "
-                                                                                 "vehicles (1k intervals)")
+    axis[1].bar(range(2000, 2013), personalVehiclesThousands, linewidth=1, label=f"Number of {attrNames[2].lower()} "
+                                                                                 "(1k intervals)")
     # Plot the graph for second subplot
-    axis[1].set_title(f"Traffic data of every 1000 personal vehicles from 2000 to 2012")
+    axis[1].set_title(f"Traffic data of every 1000 {attrNames[2].lower()} from 2000 to 2012")
     axis[1].grid()
-    axis[1].set(xlabel="year", ylabel="Number of personal vehicles")
+    axis[1].set(xlabel="year", ylabel=f"Number of {attrNames[2].lower()}")
     axis[1].xaxis.set_major_locator(tkr.MultipleLocator(1))
     axis[1].yaxis.set_minor_locator(tkr.MultipleLocator(125))
     axis[1].set_xlim(2000 - 0.5, 2012 + 0.5)
@@ -131,6 +148,36 @@ def displayPlot() -> None:
 
 
 def getUserSubInput() -> str:
+    """ Retrieves the user input for the set of available data
+
+    Using a for-each loop, the function prints out both the alphabetical index and the name of the data.
+    To iteratively print the alphabetical index, the function uses chr() function call to convert numerical
+    data to readable ASCII characters using an ASCII table, with the number 65 for ASCII 'A' being the first
+    in the index \n
+    >> ord('A') -> 65 \n
+    >> i = 1 \n
+    >> chr(i + ord('A')) -> chr(1 + 65) -> 'B' \n
+    >> i = 2 \n
+    >> chr(i + ord('A')) -> chr(2 + 65) -> 'C' \n
+    * Pressing 'Enter' with no character input will take you back to the first menu \n
+    To determine and correctly output the data chosen by the user, the function performs a similar operation as to where
+    it displays the available data options. \n
+    The function gets the user input, then attempt to match the user's input with either the non-case sensitive data
+    identifier or the alphabetical index, by using a .index() method to get the position that the data identtifier is in
+    >? Please enter your selection: b \n
+    >> userInput = "b".upper() -> "B" \n
+    >> name = "BUS PASSENGERS" \n
+    >> attrNames.index(name) -> 0 \n
+    >> charIndex = chr(0 + 65) -> 'A' \n
+    >> if userInput == name or userInput == charIndex -> 'B' != "BUS PASSENGERS" || 'B' != "A" // Evaluates to false \n
+    >> name = "BUSES" \n
+    >> attrNames.index(name) -> 1 \n
+    >> charIndex = chr(1 + 65) -> 'B' \n
+    >> if userInput == name or userInput == charIndex -> 'B' != "BUSES" || 'B' == "B" // Evalutes to true \n
+
+
+    :return: the user's input
+    """
     print(f"{'-' * 43} List of available option inputs {'-' * 43}")
     print("Options:")
     asciiA = ord('A')
@@ -142,28 +189,43 @@ def getUserSubInput() -> str:
     # keep prompting the user for input until they give valid input
     while True:
         userInput: str = input("Please enter your selection: ").upper()
-        for c in attrNames:
-            char: str = chr(attrNames.index(c) + asciiA)
-            if userInput == c.upper() or userInput == char:
-                return char
+        for name in attrNames:
+            charIndex: str = chr(attrNames.index(name) + asciiA)
+            if userInput == name.upper() or userInput == charIndex:
+                return charIndex
         if len(userInput) == 0:
             return '\n'
         print("invalid selection input, please try again")
 
 
 def showMenu() -> None:
+    """Shows the main menu with the four options for the user to select from.
+
+    Displays the name of the scenario, followed by the options as outlined by the criteria from the scenario,
+    and finally prompt the user to select an option.
+
+    :return:
+    """
     print(f"{'-' * 45} Border Crossing Vehicles 2 {'-' * 46}")
     print("Options:")
-    print(f"A: Display the number of Buses crossing the border for each year, for the 13-year period\n"
+    print(f"A: Display the number of {attrNames[1].lower()} crossing the border for each year, for the 13-year period\n"
           f"B: Display user-selected input's mean number of vehicles from 2000 to 2007 and the maximum traffic in the "
           f"8 years \n"
           f"C: Display user-selected input's year on year growth and list down years that have an increase of >5% \n"
-          f"D: Show a graph of bus passengers per bus vs year and number of personal vehicles vs year")
+          f"D: Show a graph of {attrNames[0].lower()} per {attrNames[1].lower()} vs year and "
+          f"number of {attrNames[2].lower()} vs year")
     print("Select an option above to view the data, or type 'Quit' to exit the program")
 
 
 def getUserInput() -> str:
-    # keep prompting the user for input until they give valid input
+    """ Fetches the user's input with respect to the main menu's given selections
+
+    Keeps prompting the user to give valid input. If the user selects a valid input, then one of the four functions
+    will execute depending on which option they select, or exit the program if 'Quit' is entered
+
+    :return:
+
+    """
     while True:
         userInput: str = input("Please enter your selection: ").upper()
         if userInput == 'A' or userInput == "1":
